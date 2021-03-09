53e191712e84d4e67e8c2e95f0b6f7fa046ef295
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-03-08 23:22:06 -0500

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import ParticlesBg from "particles-bg";
+import { Helmet } from "react-helmet";
 
 // Pages
 import Home from "./pages/Home";
@@ -12,6 +13,11 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Michael Beaudry</title>
+        <meta name="description" content="Personal portfolio."/>
+      </Helmet>
       <ParticlesBg color="#035096" num={10} type="circle" bg={true} />
       <Header />
       <Home />
